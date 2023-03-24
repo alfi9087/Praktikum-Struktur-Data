f90a7151ba2223a809041cee66e6ae9c58290494
alfi9087 <95138007+alfi9087@users.noreply.github.com>
2023-03-24 05:24:38 -0700

    Add files via upload

---
+++ Modul1/Hidangan.java
@@ -0,0 +1,17 @@
+package Latihan;
+
+public class Hidangan {
+    protected String namaHidangan;
+
+    public String getNamaHidangan(){
+        return namaHidangan;
+    }
+
+    public void setNamaHidangan(String namaHidanga) {
+        this.namaHidangan = namaHidanga;
+    }
+
+    public String disantap(){
+        return "Makanan Dihidangkan";
+    }
+}
